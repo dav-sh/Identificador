256bcdcafc28683fc0b082f72f9ecc46e9e08760
David <dav.llwyr@gmail.com>
2021-08-05 19:31:12 -0600

    agregue el action listener del boton con eso puedo cambiar el texto del Jlabel

--- src/window/PanelP.java
+++ src/window/PanelP.java
@@ -1,9 +1,11 @@
 package src.window;
 import javax.swing.*;
+
+import java.awt.event.*;
 import java.awt.*;
 
 
-public class PanelP extends JPanel {
+public class PanelP extends JPanel{
 
     JButton b1;
     JTextArea t1;
@@ -18,6 +20,13 @@ public class PanelP extends JPanel {
         setLayout(new GridLayout(3,1));
         b1 = new JButton("Verificar");
         b1.setPreferredSize(new Dimension(15,50));
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                l1.setText("Cambie el texto");
+            }
+        });
+
+
         t1 = new JTextArea("Ingresa aqui el texto");
         t1.setPreferredSize(new Dimension(300,200));
         l1 = new JLabel("Info...");
--- src/window/PrincipalF.java
+++ src/window/PrincipalF.java
@@ -11,4 +11,6 @@ public class PrincipalF extends JFrame{
         setVisible(true);
 
     }
+
+    
 }
